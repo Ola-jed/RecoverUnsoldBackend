5103dde69acafc46839f68dfdca2d2dde5d83399
olabijed@gmail.com <olabijed@gmail.com>
2022-06-13 09:44:10 +0100

    Dummy password reset service created

---
+++ RecoverUnsoldApi/Services/ForgotPassword/DummyForgotPasswordService.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using RecoverUnsoldApi.Data;
+using RecoverUnsoldApi.Entities;
+
+namespace RecoverUnsoldApi.Services.ForgotPassword;
+
+public class DummyForgotPasswordService : IForgotPasswordService
+{
+    private readonly DataContext _context;
+
+    public DummyForgotPasswordService(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<string> CreateResetPasswordToken(User user)
+    {
+        const string token = "123456";
+        _context.PasswordResets.Add(new PasswordReset
+        {
+            UserId = user.Id,
+            Token = token
+        });
+        await _context.SaveChangesAsync();
+        return token;
+    }
+
+    public async Task<bool> ResetUserPassword(string token, string newPassword)
+    {
+        var passwordReset = await _context.PasswordResets
+            .FirstOrDefaultAsync(p => p.Token == token);
+        if (passwordReset == null)
+        {
+            return false;
+        }
+
+        var user = await _context.Users.FindAsync(passwordReset.UserId);
+        user!.Password = BCrypt.Net.BCrypt.HashPassword(newPassword);
+        _context.Users.Update(user);
+        _context.PasswordResets.Remove(passwordReset);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
