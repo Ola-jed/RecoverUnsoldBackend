22db4c3f53dc0f0be80cc7d54ac094bb25772851
Ola-jed <olabijed@gmail.com>
2022-07-03 11:20:25 +0100

    Updated LocationReadDto field name to Location

--- RecoverUnsoldApi/Dto/OfferReadDto.cs
+++ RecoverUnsoldApi/Dto/OfferReadDto.cs
@@ -1,4 +1,4 @@
 ﻿namespace RecoverUnsoldApi.Dto;
 
 public record OfferReadDto(Guid Id,DateTime StartDate, ulong Duration, int? Beneficiaries, decimal Price, DateTime CreatedAt,
-    LocationReadDto? LocationReadDto, IEnumerable<ProductReadDto>? Products);
+    LocationReadDto? Location, IEnumerable<ProductReadDto>? Products);
