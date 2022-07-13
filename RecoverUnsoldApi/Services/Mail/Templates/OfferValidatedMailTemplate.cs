9987b5b6b86d27818eafc078872ac8133d2c8b69
Ola-jed <olabijed@gmail.com>
2022-07-13 21:38:15 +0100

    Mails templates

---
+++ RecoverUnsoldApi/Services/Mail/Templates/OfferValidatedMailTemplate.cs
@@ -0,0 +1,10 @@
+﻿namespace RecoverUnsoldApi.Services.Mail.Templates;
+
+/// <summary>
+/// Mail body to send to users when they validate an offer
+/// </summary>
+public static class OfferValidatedMailTemplate
+{
+    public const string Text = "";
+    public const string Html = "";
+}
---
+++ RecoverUnsoldApi/Services/Mail/Templates/OrderMadeMailTemplate.cs
@@ -0,0 +1,10 @@
+﻿namespace RecoverUnsoldApi.Services.Mail.Templates;
+
+/// <summary>
+/// Mail body to send to distributors when an offer is validated
+/// </summary>
+public static class OrderMadeMailTemplate
+{
+    public const string Text = "";
+    public const string Html = "";
+}
