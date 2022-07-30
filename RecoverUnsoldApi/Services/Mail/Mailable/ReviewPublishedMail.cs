ac6d06e974e02ae190f1589438eebc93a209fce5
olabijed@gmail.com <olabijed@gmail.com>
2022-07-30 17:34:09 +0100

    Implemented reviewing feature

---
+++ RecoverUnsoldApi/Services/Mail/Mailable/ReviewPublishedMail.cs
@@ -0,0 +1,56 @@
+using MimeKit;
+using RecoverUnsoldApi.Config;
+using RecoverUnsoldApi.Services.Mail.Templates;
+
+namespace RecoverUnsoldApi.Services.Mail.Mailable;
+
+public class ReviewPublishedMail: IMailable
+{
+    private readonly AppOwner _appOwner;
+    private readonly string _username;
+    private readonly string _emailAddress;
+    private readonly string _message;
+    
+    public ReviewPublishedMail(AppOwner appOwner, string username, string emailAddress, string message)
+    {
+        _appOwner = appOwner;
+        _username = username;
+        _emailAddress = emailAddress;
+        _message = message;
+    }
+    
+    public MimeMessage Build()
+    {
+        var email = new MimeMessage
+        {
+            Subject = "RecoverUnsold : Nouveau message",
+            To = { MailboxAddress.Parse(_appOwner.Email) }
+        };
+        var builder = new BodyBuilder
+        {
+            HtmlBody = GetHtmlBody(),
+            TextBody = GetPlainTextBody()
+        };
+        email.Body = builder.ToMessageBody();
+        email.ReplyTo.Add(InternetAddress.Parse(_emailAddress));
+        return email;
+    }
+
+    private string GetHtmlBody()
+    {
+        return ReviewPublishedMailTemplate.Html
+            .Replace("{name}", _appOwner.Name)
+            .Replace("{username}", _username)
+            .Replace("{emailAddress}", _emailAddress)
+            .Replace("{message}", _message);
+    }
+
+    private string GetPlainTextBody()
+    {
+        return ReviewPublishedMailTemplate.Text
+            .Replace("{name}", _appOwner.Name)
+            .Replace("{username}", _username)
+            .Replace("{emailAddress}", _emailAddress)
+            .Replace("{message}", _message);
+    }
+}
--- RecoverUnsoldApi/Services/Reviews/IReviewsService.cs
+++ RecoverUnsoldApi/Services/Reviews/IReviewsService.cs
@@ -1,11 +1,13 @@
 using FluentPaginator.Lib.Page;
 using FluentPaginator.Lib.Parameter;
 using RecoverUnsoldApi.Dto;
+using RecoverUnsoldApi.Entities;
 
 namespace RecoverUnsoldApi.Services.Reviews;
 
 public interface IReviewsService
 {
     Task<UrlPage<ReviewReadDto>> ListReviews(UrlPaginationParameter urlPaginationParameter);
-    Task PublishReview(Guid userId, string comment);
+    Task<Review> Publish(Guid userId, string comment);
+    Task Delete(Guid id);
 }
