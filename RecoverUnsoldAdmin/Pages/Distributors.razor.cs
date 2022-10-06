60a070e113925adecdeff60d18fb2be71e072cd2
olabijed@gmail.com <olabijed@gmail.com>
2022-10-06 22:08:42 +0100

    Ui updates and scroll to top added

--- RecoverUnsoldAdmin/Pages/Distributors.razor.cs
+++ RecoverUnsoldAdmin/Pages/Distributors.razor.cs
@@ -9,12 +9,12 @@ namespace RecoverUnsoldAdmin.Pages;
 public class DistributorsBase : ComponentBase
 {
     protected bool Loading { get; private set; } = true;
-    public int Page { get; set; } = 1;
+    public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 10;
     protected string? Search { get; set; }
-    protected MudTable<Distributor> Table { get; set; }
+    protected MudTable<Distributor>? Table { get; set; }
     protected HashSet<Guid> ExpandedRows { get; set; } = new();
-    
+
     [Inject]
     public NavigationManager NavigationManager { get; set; } = default!;
 
@@ -32,35 +32,19 @@ public class DistributorsBase : ComponentBase
             ExpandedRows.Add(id);
         }
     }
-    
-    protected override Task OnAfterRenderAsync(bool firstRender)
-    {
-        Table.RowsPerPage = PageSize;
-        return base.OnAfterRenderAsync(firstRender);
-    }
 
     protected async Task OnSearch()
     {
-        await Table.ReloadServerData();
+        await Table!.ReloadServerData();
     }
-    
+
     protected async Task<TableData<Distributor>> ServerData(TableState arg)
     {
-        Page = arg.Page + 1;
+        PageNumber = arg.Page + 1;
         PageSize = arg.PageSize;
-        var urlPaginationParameter = new UrlPaginationParameter(
-            PageSize,
-            Page,
-            NavigationManager.Uri,
-            nameof(Page),
-            nameof(PageSize)
-        );
-        var distributorsPage = await DistributorsService.ListDistributors(urlPaginationParameter, Search);
+        var paginationParam = new UrlPaginationParameter(PageSize, PageNumber, NavigationManager.Uri, nameof(PageNumber));
+        var distributorsPage = await DistributorsService.ListDistributors(paginationParam, Search);
         Loading = false;
-        return new TableData<Distributor>
-        {
-            Items = distributorsPage.Items,
-            TotalItems = distributorsPage.Total
-        };
+        return new TableData<Distributor> { Items = distributorsPage.Items, TotalItems = distributorsPage.Total };
     }
 }
