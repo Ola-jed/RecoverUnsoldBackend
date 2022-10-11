7938d78c8adc17795b8848fceed9e0cfe76a33c0
olabijed@gmail.com <olabijed@gmail.com>
2022-10-11 15:13:24 +0100

    Wip

--- RecoverUnsoldDomain/RecoverUnsoldDomain.GlobalUsings.g.cs
+++
@@ -1,4 +0,0 @@
-global using System.Net.Http.Json;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Logging;
