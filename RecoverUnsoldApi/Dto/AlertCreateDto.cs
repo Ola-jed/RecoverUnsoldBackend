9b4fc850c203e75eae2f6950e3d6d97cbce0d0fb
olabijed@gmail.com <olabijed@gmail.com>
2023-07-27 21:07:14 +0100

    wip

--- RecoverUnsoldApi/Dto/AlertCreateDto.cs
+++ RecoverUnsoldApi/Dto/AlertCreateDto.cs
@@ -2,5 +2,5 @@ using System.ComponentModel.DataAnnotations;
 using RecoverUnsoldDomain.Entities.Enums;
 
 namespace RecoverUnsoldApi.Dto;
-// TODO : validate that the distributor id really exists
+
 public record AlertCreateDto([Required] AlertType AlertType, Guid? DistributorId = null);
