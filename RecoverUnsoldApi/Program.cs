62619c722a3a984f751aa97583c0f781fb1c3eeb
Ola-jed <olabijed@gmail.com>
2023-10-17 21:25:55 +0100

    Removed modification of port

--- RecoverUnsoldApi/Program.cs
+++ RecoverUnsoldApi/Program.cs
@@ -43,12 +43,6 @@ builder.Services.AddServices();
 builder.Services.ConfigureAuthentication(configuration);
 
 var app = builder.Build();
-if (app.Environment.IsProduction())
-{
-    var port = Environment.GetEnvironmentVariable("PORT");
-    app.Urls.Add($"http://*:{port}");
-}
-
 app.UseStaticFiles();
 app.UseSwagger();
 var currentAssembly = Assembly.GetAssembly(typeof(Program))!;
